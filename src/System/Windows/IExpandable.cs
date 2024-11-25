b2c8190b17de2921d2713bf28da8f5b9389e2414
IvanGit <5630565+IvanGit@users.noreply.github.com>
2024-11-25 07:09:38 +0300

    v0.2.2

---
+++ src/System/Windows/IExpandable.cs
@@ -0,0 +1,39 @@
+﻿namespace System.Windows
+{
+    /// <summary>
+    /// Defines a contract for objects that can be expanded and collapsed.
+    /// </summary>
+    public interface IExpandable
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the object is expanded.
+        /// </summary>
+        public bool IsExpanded { get; set; }
+
+        /// <summary>
+        /// Collapses the object by setting the <see cref="IsExpanded"/> property to <c>false</c>.
+        /// </summary>
+
+#if NETSTANDARD2_1_OR_GREATER || NET6_0_OR_GREATER
+        public void Collapse()
+        {
+            IsExpanded = false;
+        }
+#else
+        public void Collapse();
+#endif
+
+        /// <summary>
+        /// Expands the object by setting the <see cref="IsExpanded"/> property to <c>true</c>.
+        /// </summary>
+
+#if NETSTANDARD2_1_OR_GREATER || NET6_0_OR_GREATER
+        public void Expand()
+        {
+            IsExpanded = true;
+        }
+#else
+        public void Expand();
+#endif
+    }
+}
